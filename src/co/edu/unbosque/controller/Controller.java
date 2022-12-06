25c4f3f9fda38494bb231c4dad80b085e8248599
monosusio <ssanabriag@unbosque.edu.co>
2022-12-05 20:36:46 -0500

    sexo anal

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,16 +1,76 @@
 package co.edu.unbosque.controller;
 
+import java.util.Scanner;
+
 import co.edu.unbosque.model.ClaseX;
 import co.edu.unbosque.view.View;
 
 public class Controller {
-	
+
+	Scanner sc = new Scanner(System.in);
 	private ClaseX m;
 	private View gui;
-	
+
 	public Controller() {
-		m = new ClaseX();
-		gui = new View();
-		gui.mostrarResultados(m.procesarDato(gui.pedirDato()));
+
+		inicializar();
+		
+	}
+
+	public void inicializar() {
+
+		boolean done = false;
+
+		do {
+			System.out.println("Digite el punto que desea visualizar");
+			System.out.println("1. Programación Dinámica");
+			System.out.println("2. Backtracking");
+			System.out.println("3. Rama y Poda");
+			System.out.println("4. Salir");
+
+			int dato = sc.nextInt();
+
+			switch (dato) {
+			case 1:
+				int num = 0;
+				System.out.println("¿Cuantos platos desea ingresar?");
+				int numplatos = sc.nextInt();
+				int platos[] = new int[numplatos];
+				for(int i=0;i<numplatos;i++) {
+					System.out.println("Digite las calorias del plato numero "+ (i+1));
+					int valor =sc.nextInt();
+					platos[i]=valor;	
+					num +=valor;
+				}
+				System.out.println("Maximas calorias posibles "+ num);
+				System.out.println("Cuantas calorias desea consumir ");
+				int calorias = sc.nextInt();
+				if(num<calorias) {
+					System.out.println("insuficientes calorias en los platos para llegar a la meta");
+				}else {
+				m.setI(num+1);
+				m.Resolver(platos, calorias);
+				}
+				break;
+			case 2:
+
+				System.out.println("Proximamente");
+
+				break;
+				
+			case 3:
+
+			case 4:
+				
+				System.out.println("Gracias, vuelva pronto...");
+				done = true;
+				
+				break;
+				
+			default:
+				System.out.println("Inválido. Ingrese una opción entre (1-3).");
+			}
+
+		} while (!done);
 	}
 }
--- src/co/edu/unbosque/model/ClaseX.java
+++ src/co/edu/unbosque/model/ClaseX.java
@@ -1,27 +1,133 @@
 package co.edu.unbosque.model;
 
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
+
 public class ClaseX {
-	ClaseY y;
+
+	public static int I = Integer.MAX_VALUE;
+
+	static int Min(int a, int b) {
+		return (b < a) ? b : a;
+	}
+
+	public static int[][] Calcular(int[] lista, int N, int C) {
+		
+		int MaxElem = N + 1;
+		int MaxCant = C + 1;
+		
+		//establece el tamaño de la matriz 
+		int[][] tabla = new int[MaxElem][MaxCant];
+
+		//llena la matriz inicial
+		for (int i = 0; i < MaxElem; i++) {
+			tabla[i][0] = 0;
+		}
+		for (int i = 1; i < MaxCant; i++) {
+			tabla[0][i] = I;
+		}
+
+		
+		
+		int a, b;
+		for (int i = 1; i < MaxElem; i++) {
+			for (int j = 1; j < MaxCant; j++) {
+				a = tabla[i - 1][j];
+				if (lista[i - 1] <= j) {
+					b = tabla[i - 1][j - lista[i - 1]];
+					if (b < I)
+						b += lista[i - 1];
+				} else {
+					b = lista[i - 1];
+				}
+				if (b >= j) {
+					tabla[i][j] = Min(a, b);
+				} else {
+					tabla[i][j] = I;
+				}
+			}
+		}
+		return tabla;
+	}
+
+	private static Object[] Resultado(int[][] t, int[] l, int N, int C) {
+		List<Integer> r = new ArrayList<Integer>();
+		int i = N;
+		int j = C;
+
+		while (i > 0 && j > 0) {
+			if (t[i][j] == t[i - 1][j]) {
+				i--;
+			} else {
+				r.add(i - 1);
+				j -= l[i - 1];
+				i--;
+			}
+		}
+		return r.toArray();
+	}
+
+	public static void Resolver(int[] lista, int C) {
+		
+		int[][] tabla = Calcular(lista, lista.length, C);
+
+		for (int i = 0; i < tabla.length; ++i) {
+			for (int j = 0; j < tabla[0].length; ++j) {
+				System.out.print("[" + tabla[i][j] + "]");
+			}
+			System.out.println();
+		}
+		Object[] r = Resultado(tabla, lista, lista.length, C);
+
+		System.out.println("Lista :");
+		for (int i : lista) {
+			System.out.print("[" + i + "]");
+		}
+
+		System.out.println();
+
+		Formatter f = new Formatter();
+		f.format("Total a consumir: %s calorías", C);
+		System.out.println(f.toString());
+
+		int suma = 0;
+
+		for (Object n : r) {
+			int valor = (Integer) n;
+			suma += lista[valor];
+			Formatter f1 = new Formatter();
+			f1.format("Plato %s = %s calorías", valor + 1, lista[valor]);
+			System.out.println(f1.toString());
+		}
+		Formatter f2 = new Formatter();
+		f2.format("Total consumido:  %s calorías", suma);
+		System.out.println(f2.toString());
+
+	}
+
+	public static int getI() {
+		return I;
+	}
+
+	public static void setI(int i) {
+		I = i;
+	}
 	
-	public ClaseX() {
-		y = new ClaseY();
-	}
-
-	public String procesarDato(String dato) {
-		return dato + " Proceso en ClaseX";
-	}
-
-	public int sumar20alDato(int dato) {
-		return dato+20;
-	}
 	
-	public ClaseY getY() {
-		return y;
-	}
 
-	public void setY(ClaseY y) {
-		this.y = y;
-	}
+				
+//				int[] lista = { 7, 8, 9, 4, 5, 6 };
+//				Resolver(lista, 10);
+//				Resolver(lista, 37);
+//				
+//				int[] lista2 = { 11, 9, 5, 3 };
+//				Resolver(lista2, 18);
+//				Resolver(lista2, 15);
+//
+//				int[] lista3 = { 8, 16, 24, 32 };
+//				Resolver(lista3, 27);
+//				Resolver(lista3, 42);
 
 	
 }
